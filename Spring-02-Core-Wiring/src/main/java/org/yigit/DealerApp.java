6775c6048d196e980d7056547a6da1eca087c326
Gokhan Yigit <yigitgokhan1@gmail.com>
2023-08-28 23:25:06 +0200

    Add Wiring Samples

---
+++ Spring-02-Core-Wiring/src/main/java/org/yigit/DealerApp.java
@@ -0,0 +1,17 @@
+package org.yigit;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class DealerApp {
+    public static void main(String[] args) {
+        ApplicationContext context= new AnnotationConfigApplicationContext(ConfigCar.class);
+
+        Car c= context.getBean(Car.class);
+        Person p= context.getBean(Person.class);
+
+        System.out.println(c.getMake());
+        System.out.println(p.getCar().getMake());
+        System.out.println(p.getName());
+    }
+}
---
+++ Spring-02-Core-Wiring/src/main/java/org/yigit/Person.java
@@ -0,0 +1,13 @@
+package org.yigit;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Person {
+    private String name;
+    private Car car;
+}
