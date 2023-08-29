2337cffb6c1aed1ca7d24eeabde6aae57e1008bb
Gokhan Yigit <yigitgokhan1@gmail.com>
2023-08-29 12:42:05 +0200

    Add Depedency Injection Samples

---
+++ Spring-03-Core-DI/src/main/java/org/yigit/ConfigHours.java
@@ -0,0 +1,9 @@
+package org.yigit;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class ConfigHours {
+}
---
+++ Spring-03-Core-DI/src/main/java/org/yigit/OfficeHours.java
@@ -0,0 +1,10 @@
+package org.yigit;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OfficeHours {
+    public int getHours(){
+        return 5;
+    }
+}
