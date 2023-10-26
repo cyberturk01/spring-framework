8df136963944a151b644581002dcc3ba68a58cbe
Gokhan Yigit <yigitgokhan1@gmail.com>
2023-10-26 12:29:00 +0200

    ORM Mapping with Region Table

--- Spring-11-ORM-Mapping-OneToOne/src/main/java/org/yigit/bootstrap/GenerateData.java
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/org/yigit/bootstrap/GenerateData.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.yigit.entity.Department;
 import org.yigit.entity.Employee;
+import org.yigit.entity.Region;
 import org.yigit.enums.Gender;
 import org.yigit.repository.DepartmentsRepository;
 import org.yigit.repository.EmployeesRepository;
@@ -35,14 +36,21 @@ public class GenerateData implements CommandLineRunner {
         Employee employee3 = new Employee("Sydney","Symonds","ssymonds2@hhs.gov", LocalDate.of(2010,5,17), Gender.FEMALE, BigInteger.valueOf(204864));
         Employee employee4 = new Employee("Avrom","Rowantree",null, LocalDate.of(2014,8,2), Gender.MALE, BigInteger.valueOf(150864));
 
+        Region region1  = new Region("Southwest","United States");
+        Region region2  = new Region("Northeast","United States");
+        Region region3  = new Region("Central","Asia");
+        Region region4  = new Region("Frankfurt","Germany");
+
+
         employee1.setDepartment(department1);
         employee2.setDepartment(department2);
         employee3.setDepartment(department3);
         employee4.setDepartment(department4);
 
-//        departmentsRepository.save(department1);
-//        departmentsRepository.save(department2);
-//        departmentsRepository.save(department3);
+        employee1.setRegion(region1);
+        employee2.setRegion(region2);
+        employee3.setRegion(region3);
+        employee4.setRegion(region4);
 
         employeesRepository.save(employee1);
         employeesRepository.save(employee2);
